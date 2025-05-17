6957f1fc176bcbbc9961c3634d034e6992c7678d
BerniSc <bernhard.schoeffel@gmail.com>
2025-05-17 22:10:30 +0200

    Moved Bannerconfig to own file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -177,37 +177,7 @@ vim.keymap.set('n', '<leader>w', function()
 end)
 
 -- Setup the Banner to display relevant Stuff under the Editor
-require('lualine').setup {
-    options = {
-        theme = 'auto',
-        component_separators = '|',
-        section_separators = '',
-        -- Disable for NvimTree and other file types that dont require it
-        disabled_filetypes = {
-            'NvimTree',
-            'packer',
-            'help'
-        },
-        -- We could set this if we just want one Status, for now leave it of and see how much it bothers
-        globalstatus = false,
-        -- Only display if it fits...
-        cond = function()
-            return vim.o.columns > 50
-        end
-    },
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {
-            {'branch'},
-            {'diff'},
-        },
-        lualine_c = {{'filename', path = 1}},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
-    }
-}
-
+require('config/banner-config')
 
 -- LSP Setup
 require('config/lsp-config')
---
+++ nvim/.config/nvim/lua/config/banner-config.lua
@@ -0,0 +1,30 @@
+require('lualine').setup {
+    options = {
+        theme = 'auto',
+        component_separators = '|',
+        section_separators = '',
+        -- Disable for NvimTree and other file types that dont require it
+        disabled_filetypes = {
+            'NvimTree',
+            'packer',
+            'help'
+        },
+        -- We could set this if we just want one Status, for now leave it of and see how much it bothers
+        globalstatus = false,
+        -- Only display if it fits...
+        cond = function()
+            return vim.o.columns > 50
+        end
+    },
+    sections = {
+        lualine_a = {'mode'},
+        lualine_b = {
+            {'branch'},
+            {'diff'},
+        },
+        lualine_c = {{'filename', path = 1}},
+        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+    }
+}
