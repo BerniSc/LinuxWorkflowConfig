39d18c89c929b7fd11f1eeacd15e61647b8b34d9
BerniSc <bernhard.schoeffel@gmail.com>
2025-05-18 19:12:00 +0200

    Added conditional ESP Mode for clangd

--- nvim/.config/nvim/lua/config/lsp-config.lua
+++ nvim/.config/nvim/lua/config/lsp-config.lua
@@ -23,18 +23,33 @@ require("mason-lspconfig").setup({
 local lspconfig = require('lspconfig')
 
 -- Clangd setup
-lspconfig.clangd.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    cmd = { 
-        "clangd",
-        "--background-index",
-        "--clang-tidy",
-        "--header-insertion=iwyu",
-        "--completion-style=detailed",
-        "--function-arg-placeholders=false"
-    }
-})
+-- see https://github.com/espressif/esp-idf/issues/6721#issuecomment-2231830343
+-- Gist is: As soon as we export get_idf for our nvim shell instance we go into special esp compiler mode
+local esp_idf_path = os.getenv("IDF_PATH")
+if esp_idf_path then
+  -- for esp-idf
+    lspconfig.clangd.setup({
+        handlers = handlers,
+        capabilities = capabilities;
+        cmd = { "/home/berni/.espressif/tools/esp-clang/16.0.1-fe4f10a809/esp-clang/bin/clangd", "--background-index", "--query-driver=**", },
+        root_dir = function()
+            -- leave empty to stop nvim from cd'ing into ~/ due to global .clangd file
+        end
+    })
+else
+    lspconfig.clangd.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders=false"
+        }
+    })
+end
 
 -- Svelte setup
 lspconfig.svelte.setup({
